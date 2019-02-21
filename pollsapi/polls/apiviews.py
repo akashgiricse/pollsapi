cf75248cf9a2be3f6c389ef28688cec266dfccb4
akashgiricse <akashgiricse@gmail.com>
2019-02-21 13:37:45 +0530

    Add destroy and post method to PollViewSet and ChoiceList respectively

--- pollsapi/polls/apiviews.py
+++ pollsapi/polls/apiviews.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, status, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 
 from django.contrib.auth import authenticate
 from .models import Poll, Choice
@@ -11,13 +12,25 @@ class PollViewSet(viewsets.ModelViewSet):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
 
+    def destroy(self, request, *args, **kwargs):
+        poll = Poll.objects.get(pk=self.kwargs["pk"])
+        if not request.user == poll.created_by:
+            raise PermissionDenied("You can not delete this poll.")
+        return super().destroy(request, *args, **kwargs)
+
 
 class ChoiceList(generics.ListCreateAPIView):
+    serializer_class = ChoiceSerializer
 
     def get_queryset(self):
         queryset = Choice.objects.filter(poll_id=self.kwargs["pk"])
         return queryset
-    serializer_class = ChoiceSerializer
+
+    def post(self, request, *args, **kwargs):
+        poll = Poll.objects.get(pk=self.kwargs["pk"])
+        if not request.user == poll.created_by:
+            raise PermissionDenied("You can not create choice for this poll.")
+        return super().post(request, *args, **kwargs)
 
 
 class CreateVote(APIView):
